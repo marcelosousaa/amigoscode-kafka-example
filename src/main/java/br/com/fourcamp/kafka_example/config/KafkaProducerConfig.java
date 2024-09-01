c70d4451cebc51b3f1e15fae5647cb29adb50ed4
Marcelo de Sousa Silva <sousams93@outlook.com>
2024-08-31 23:43:46 -0300

    update KafkaProducer.java file

--- src/main/java/br/com/fourcamp/kafka_example/config/KafkaProducerConfig.java
+++ src/main/java/br/com/fourcamp/kafka_example/config/KafkaProducerConfig.java
@@ -13,13 +13,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class KafkaProducer {
+public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
     public Map<String, Object> producerConfig(){
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers );
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
